9687669dbf274cf864ad72a325115ef0dad75145
Noah Hazan <72990446+noah-hazan-repo@users.noreply.github.com>
2022-07-19 12:59:49 -0400

    refactor and add comments

--- BirthdayBot/BirthdayBot.py
+++ BirthdayBot/BirthdayBot.py
@@ -9,67 +9,49 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 
-def createBrowser():
+def birthdayBot():
+    username = '---FILL THIS IN---'
+    password = '---FILL THIS IN---'
+    # modify browser options to dissalow popups which break selenium
     option = Options()
-
     option.add_argument("--disable-infobars")
-
     option.add_argument("start-maximized")
-
-    option.add_argument("--disable-extensions")
-
+    option.add_argument("--disable-extensions") 
     driv = ChromeDriverManager().install() 
-
     option.add_experimental_option(
         "prefs", {"profile.default_content_setting_values.notifications": 1})
-
+    # instantiate browser
     browser = webdriver.Chrome(driv, chrome_options=option)
-    
-    return browser
-
-def getBirthdays(username, password):
-    
-    browser = createBrowser()
-
     browser.get('https://www.facebook.com/birthdays/')
-
+    # login
     browser.find_element(By.CSS_SELECTOR,'#email').send_keys(f'{username}')
-
     browser.find_element(By.CSS_SELECTOR,'#pass').send_keys(f'{password}')
-
     browser.find_element(By.NAME,'login').click()
-    
+    # sleep since moving too quickly seems to break program
+    time.sleep(5)
+    # grab all names and birthday message boxes (lists)
     names_dates = browser.find_elements(By.CSS_SELECTOR,'.o8rfisnq .i1fnvgqd')
-    
-    birthdays = browser.find_elements(By.CSS_SELECTOR,'._1mj') #list
-    
+    birthdays = browser.find_elements(By.CSS_SELECTOR,'._1mj') 
+    # clean names
     names = []
-
     for name in names_dates:
-
         names.append(name.text.split('\n')[0].split(' ')[0])
-
     names = names[:len(birthdays)]
-    
-    print(names,birthdays)
-    
-    return names,birthdays
-
-
-def birthdayBot(names,birthdays):
-    
+    # assert names and birthdays are aligned
+    # if false, it means bday messages would go to people with the wrong name in the message 
+    # thus, we can't allow the below assertion to not be true
     assert len(names) == len(birthdays)
-
-    for i in range(len(birthdays)):
-
-        name = names[i].text.split('\n')[0].split(' ')[0]
-
-        birthday = birthdays[i]
-
-        birthday.send_keys(f'Happy Birthday, {name}!')
-
-        birthday.click()
-
-        birthday.send_keys(Keys.RETURN)
-
-birthdayBot(getBirthdays(username,password))
+    # zip names and selenium objects for message boxes into dict
+    people = dict(zip(names,birthdays))
+    # iterate through people and send them a birthday message
+    for name, birthday in people.items():
+    # hard coded one person I actually wanted to wish a bday on the day of testing (jul 19) ;)
+        if name == 'Leora': 
+            birthday.send_keys(f'Happy Birthday {name}!')
+    # sleep again since moving too quickly breaks program
+            time.sleep(5)
+            birthday.click()
+            birthday.send_keys(Keys.RETURN)
+            
+# Execute            
+birthdayBot()
